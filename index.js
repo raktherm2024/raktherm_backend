ba593bd8aea8a103c82682f9aaebe3469ccf18a9
zardron <zardron.pesquera1993@gmail.com>
2024-04-02 11:26:15 +0400

    adding some modification

--- index.js
+++ index.js
@@ -12,16 +12,28 @@ dotenv.config();
 dbConn();
 const port = process.env.PORT || 5000;
 const app = express();
-app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
+const corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "https://raktherm-backend.vercel.app",
+  ],
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use("/api/customers", customerRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on PORT: ${port}`);
