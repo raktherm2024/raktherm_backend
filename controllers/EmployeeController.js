d3766fd63d493152a56adc620116349a559647da
zardron <zardron.pesquera1993@gmail.com>
2024-04-24 15:56:03 +0400

    adding remove employee function

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -17,6 +17,29 @@ const getEmployee = async (req, res) => {
   res.status(200).json(employee);
 };
 
+const getSpecificEmployee = async (req, res) => {
+  const { id } = req.params;
+
+  const employee = await Employee.aggregate([
+    {
+      $match: {
+        $expr: { $eq: ["$_id", { $toObjectId: id }] },
+      },
+    },
+    {
+      $lookup: {
+        from: "auths",
+        localField: "_id",
+        foreignField: "user",
+        pipeline: [{ $project: { _id: 0, email: 1, password: 1 } }],
+        as: "account",
+      },
+    },
+  ]);
+
+  res.status(200).json(employee);
+};
+
 const addEmployee = async (req, res) => {
   const { employeeCode, employeeName, location, type, email, password } =
     req.body;
@@ -37,6 +60,16 @@ const addEmployee = async (req, res) => {
 
   if (checkEmployee && checkAccount)
     return res.status(400).json({ message: "Employee is already exist" });
+
+  if (checkEmployee)
+    return res.status(400).json({
+      message: "Employee Code is already exist.",
+    });
+
+  if (checkAccount)
+    return res.status(400).json({
+      message: "Email is already exist.",
+    });
 
   const newEmployee = await Employee.create({
     employeeCode,
@@ -59,10 +92,42 @@ const removeEmployee = async (req, res) => {
   const { id } = req.params;
 
   const remove = await Employee.findByIdAndDelete(id);
+  const authDetails = await Auth.findOne({ user: id });
+
+  await Auth.findByIdAndDelete(authDetails._id);
 
   if (remove) {
     res.status(200).json({ message: "Employee has been removed!" });
   }
 };
 
-export { getEmployee, addEmployee, removeEmployee };
+const updateEmployee = async (req, res) => {
+  const { id } = req.params;
+  const { employeeCode, employeeName, location, type, email, password } =
+    req.body;
+
+  const update = await Employee.findByIdAndUpdate(
+    id,
+    {
+      employeeCode: employeeCode,
+      employeeName: employeeName,
+      location: location,
+      type: type,
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (update) {
+    res.status(200).json({ message: "Employee has been updated." });
+  }
+};
+
+export {
+  getEmployee,
+  addEmployee,
+  removeEmployee,
+  getSpecificEmployee,
+  updateEmployee,
+};
--- routes/EmployeeRoutes.js
+++ routes/EmployeeRoutes.js
@@ -2,13 +2,17 @@ import express from "express";
 import {
   addEmployee,
   getEmployee,
+  getSpecificEmployee,
   removeEmployee,
+  updateEmployee,
 } from "../controllers/EmployeeController.js";
 
 const router = express.Router();
 
 router.get("/", getEmployee);
 router.post("/", addEmployee);
+router.post("/:id", getSpecificEmployee);
+router.put("/:id", updateEmployee);
 router.delete("/:id", removeEmployee);
 
 export default router;
