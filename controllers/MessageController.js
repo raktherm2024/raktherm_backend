30e140ef32482896d77bb457ef8538b846bc00e3
zardron <zardron.pesquera1993@gmail.com>
2024-04-30 10:01:15 +0400

    fix sending sms

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -1,6 +1,8 @@
 import Message from "../model/MessageModel.js";
 import { Infobip, AuthType } from "@infobip-api/sdk";
 import dotenv from "dotenv";
+import axios from "axios";
+
 dotenv.config();
 
 const infobipClient = new Infobip({
@@ -34,19 +36,31 @@ export const sendMessage = async (req, res) => {
   const { contactList, messageContent } = req.body;
 
   for (let index = 0; index < contactList.length; index++) {
-    const send = await infobipClient.channels.sms.send({
-      type: "text",
-      messages: [
-        {
-          destinations: [
-            {
-              to: contactList[index],
-            },
-          ],
-          text: messageContent,
-        },
-      ],
+    await axios.post("https://api.smsala.com/api/SendSMS/", {
+      api_id: "API1709394012144",
+      api_password: "iMcS1vPX1V",
+      sms_type: "T",
+      encoding: "T",
+      sender_id: "AUTOSERVICE",
+      phonenumber: contactList[index],
+      textmessage: messageContent,
     });
+    // await infobipClient.channels.sms.send(
+    //   {
+    //     type: "text",
+    //     messages: [
+    //       {
+    //         destinations: [
+    //           {
+    //             to: contactList[index],
+    //           },
+    //         ],
+    //         from: "RAKtherm",
+    //         text: messageContent,
+    //       },
+    //     ],
+    //   },
+    //   {}
   }
   res.status(200).json({ message: "Messages successfully sent" });
 };
